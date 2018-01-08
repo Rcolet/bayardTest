57273fc23f61d01aaac6c7489ee9938596298c1e
Rcolet <remi.colet@icloud.com>
2018-01-08 17:26:26 +0100

    Mise ne place d'un évènement avec des services

---
+++ src/BayardTest/PlatformBundle/DoctrineListener/ApplicationCreationListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace BayardTest\PlatformBundle\DoctrineListener;
+
+use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
+use BayardTest\PlatformBundle\Email\ApplicationMailer;
+use BayardTest\PlatformBundle\Entity\Application;
+
+class ApplicationCreationListener
+{
+    /**
+     * @var ApplicationMailer
+     */
+    private $applicationMailer;
+
+    public function __construct(ApplicationMailer $applicationMailer)
+    {
+        $this->applicationMailer = $applicationMailer;
+    }
+
+    public function postPersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getObject();
+
+        // On ne veut envoyer un email que pour les entités Application
+        if (!$entity instanceof Application) {
+          return;
+        }
+
+        $this->applicationMailer->sendNewNotification($entity);
+    }
+}
