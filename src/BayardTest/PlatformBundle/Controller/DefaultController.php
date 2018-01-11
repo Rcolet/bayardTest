46f888f7a197a49d060ef25d8333707380ee7758
Rcolet <remi.colet@icloud.com>
2018-01-11 11:49:26 +0100

    Ajout de fonctions et d'une view de suppression d'annonce et d'image

--- src/BayardTest/PlatformBundle/Controller/DefaultController.php
+++ src/BayardTest/PlatformBundle/Controller/DefaultController.php
@@ -66,103 +66,26 @@ class DefaultController extends Controller
     }
 
     /**
+     * @Route("/view/images", name="oc_platform_view_images")
+     */
+    public function viewImagesAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $images = $em->getRepository('BayardTestPlatformBundle:Image')->findAll();
+
+        if (null === $images) {
+            throw new NotFoundHttpException("la table application est vide");
+        }
+
+        return $this->render('@BayardTestPlatform/Default/viewImages.html.twig', array('images' => $images));
+    }
+
+    /**
      * @Route("/add", name="oc_platform_add")
      */
     public function addAction(Request $request)
     {   
-        /*// Création de l'entité Advert
-        $advert = new Advert();
-        $advert->setTitle('Recherche développeur Symfony.');
-        $advert->setAuthor('Alexandre');
-        $advert->setContent("Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…");
-
-        // Création de l'entité Image
-        $image = new Image();
-        $image->setUrl('http://sdz-upload.s3.amazonaws.com/prod/upload/job-de-reve.jpg');
-        $image->setAlt('Job de rêve');
-
-        // On lie l'image à l'annonce
-        $advert->setImage($image);
-
-        // Création d'une première candidature
-        $application1 = new Application();
-        $application1->setAuthor('Marine');
-        $application1->setContent("J'ai toutes les qualités requises.");
-        $application1->setDate(new \DateTime());
-
-        // Création d'une deuxième candidature par exemple
-        $application2 = new Application();
-        $application2->setAuthor('Pierre');
-        $application2->setContent("Je suis très motivé.");
-        $application2->setDate(new \DateTime());
-
-
-        // On lie les candidatures à l'annonce
-        $advert->addApplication($application1);
-        $advert->addApplication($application2);
-
-        $category1 = new Category();
-        $category1->setName("Web");
-        $category2 = new Category();
-        $category2->setName("BD");
-
-        // On boucle sur les catégories pour les lier à l'annonce
-        $advert->addCategory($category1);
-        $advert->addCategory($category2);
-
-        // Liste des noms de compétences à ajouter
-        $names = array('PHP', 'Symfony', 'C++', 'Java', 'Photoshop', 'Blender', 'Bloc-note');
-
-        // On récupère l'EntityManager
-        $em = $this->getDoctrine()->getManager();
-
-        foreach ($names as $name) {
-
-            // On crée la compétence
-            $skill = new Skill();
-            $skill->setName($name);
-
-            // On crée une nouvelle « relation entre 1 annonce et 1 compétence »
-            $advertSkill = new AdvertSkill();
-
-            // On la lie à l'annonce, qui est ici toujours la même
-            $advertSkill->setAdvert($advert);
-
-            // On la lie à la compétence, qui change ici dans la boucle foreach
-            $advertSkill->setSkill($skill);
-
-            // Arbitrairement, on dit que chaque compétence est requise au niveau 'Expert'
-            $advertSkill->setLevel('Expert');
-
-            // On la persiste
-            $em->persist($skill);
-
-            // Et bien sûr, on persiste cette entité de relation, propriétaire des deux autres relations
-            $em->persist($advertSkill);
-        }
-
-        // Étape 1 : On « persiste » l'entité
-        $em->persist($advert);
-
-        // Étape 1 bis : si on n'avait pas défini le cascade={"persist"},
-        // on devrait persister à la main l'entité $image
-        // $em->persist($image);
-
-        // Étape 1 ter : pour cette relation pas de cascade lorsqu'on persiste Advert, car la relation est
-        // définie dans l'entité Application et non Advert. On doit donc tout persister à la main ici.
-        $em->persist($application1);
-        $em->persist($application2);
-
-        $em->persist($category1);
-        $em->persist($category2);
-
-        // Étape 2 : On déclenche l'enregistrement
-        $em->flush();
-
-        // Si on n'est pas en POST, alors on affiche le formulaire
-        return $this->redirectToRoute('oc_platform_view');*/
-
-
         $advert = new Advert();
 
         $form = $this->createForm(AdvertType::class, $advert);
@@ -210,10 +133,42 @@ class DefaultController extends Controller
      */
     public function removeAdvertAction(Request $request, $id)
     {
-
         $em = $this->getDoctrine()->getManager();
 
         $advert = $em->getRepository('BayardTestPlatformBundle:Advert')->find($id);
+        $em->remove($advert);
+        $em->flush();
+
+        return $this->redirectToRoute('oc_platform_view');
+    }
+
+    /**
+     * @Route("/remove/image/adverts/{id}", name="oc_platform_remove_image_adverts")
+     */
+    public function removeImageByAdvertsAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $adverts = $em->getRepository('BayardTestPlatformBundle:Advert')->findByImage($id);
+        foreach ($adverts as $advert) {
+            $advert->setImage(null);
+        }
+        $image = $em->getRepository('BayardTestPlatformBundle:Image')->find($id);
+        $em->remove($image);
+        
+        $em->flush();
+
+        return $this->redirectToRoute('oc_platform_view');
+    }
+
+    /**
+     * @Route("/remove/image/{id}", name="oc_platform_remove_image")
+     */
+    public function removeImageAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $advert = $em->getRepository('BayardTestPlatformBundle:Image')->find($id);
         $em->remove($advert);
         $em->flush();
 
