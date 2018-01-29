750b0a41d6c465f04d3cc5911f2e584366364d33
Rcolet <remi.colet@icloud.com>
2018-01-29 10:59:44 +0100

    Finalisation des formulaire imbriquer

--- src/BayardTest/PlatformBundle/Form/AdvertType.php
+++ src/BayardTest/PlatformBundle/Form/AdvertType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class AdvertType extends AbstractType
 {
@@ -25,7 +26,11 @@ class AdvertType extends AbstractType
             ->add('content',    TextareaType::class)
             ->add('published',  CheckboxType::class, array('required' => false))
             ->add('image',      ImageType::class)
-            // ->add('categories')
+            ->add('categories', CollectionType::class, array(
+                    'entry_type'   => CategoryType::class,
+                    'allow_add'    => true,
+                    'allow_delete' => true
+                ))
             ->add('save',       SubmitType::class);
     }/**
      * {@inheritdoc}
---
+++ src/BayardTest/PlatformBundle/Form/CategoryType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace BayardTest\PlatformBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoryType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BayardTest\PlatformBundle\Entity\Category'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'bayardtest_platformbundle_category';
+    }
+
+
+}
